842bea23ad1298a56b761b03cdee35baadf23fa8
Cristian Rojas <injcristianrojas@gmail.com>
2022-08-25 23:46:05 -0400

    Login query endpoint

--- api/app.js
+++ api/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const db = require("./db.js");
 
+app.use(express.urlencoded());
+
 app.get("/testAPI", function (req, res) {
-  res.send("API is working properly");
+  res.send("API is working properly")
 });
 
 app.get("/", function (req, res) {
@@ -23,6 +25,26 @@ app.get("/api/users", (req, res) => {
       "data": rows
     })
   });
+});
+
+app.post("/api/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const sql = "select * from users where username = '" + username + "' and password = '" + password + "'";
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(400).json({ "error": err.message });
+      return;
+    }
+    if (rows.length < 1) {
+      res.status(401).json({ "error": "unauthorized" });
+      return;
+    }
+    res.send("" + rows.length);
+  });
+
 });
 
 app.listen(9000, () => {
