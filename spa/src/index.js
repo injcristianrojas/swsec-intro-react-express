f2d4818af715321bfaac236eaefea76c33fe3b67
Cristian Rojas <injcristianrojas@gmail.com>
2022-09-04 00:09:50 -0400

    Index fixes

--- spa/src/index.js
+++ spa/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
 import Login from './Login';
 import Menu from './Menu';
 import * as serviceWorker from './serviceWorker';
